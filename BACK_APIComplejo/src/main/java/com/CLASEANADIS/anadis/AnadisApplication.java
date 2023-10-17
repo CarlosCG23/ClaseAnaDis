f3aefff8ee71673ff09066e7406761eeb1426fe0
carlosCG23 <carlos.castillog7@gmail.com>
2023-10-17 20:17:39 +0200

    Scaffolding del proyecto back (API)

---
+++ BACK_APIComplejo/src/main/java/com/CLASEANADIS/anadis/AnadisApplication.java
@@ -0,0 +1,13 @@
+package com.CLASEANADIS.anadis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnadisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnadisApplication.class, args);
+	}
+
+}
