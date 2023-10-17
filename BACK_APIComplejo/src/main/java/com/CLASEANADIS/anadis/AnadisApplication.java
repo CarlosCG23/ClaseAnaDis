d14f6e0e807021f9b89f285ca610f70bac253c8b
carlosCG23 <carlos.castillog7@gmail.com>
2023-10-17 20:34:09 +0200

    Terminada la lectura del json complejo

--- BACK_APIComplejo/src/main/java/com/CLASEANADIS/anadis/AnadisApplication.java
+++ BACK_APIComplejo/src/main/java/com/CLASEANADIS/anadis/AnadisApplication.java
@@ -7,6 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AnadisApplication {
 
 	public static void main(String[] args) {
+		/*
+		LectorJSON lectorJSON = new LectorJSON();
+		DatosJSONcomplejoPADRE datosJSONcomplejoPADRE = new DatosJSONcomplejoPADRE();
+		datosJSONcomplejoPADRE = lectorJSON.LecturaJSONcomplejo();
+		System.out.println(datosJSONcomplejoPADRE);
+		*/
+
 		SpringApplication.run(AnadisApplication.class, args);
 	}
 
--- BACK_APIComplejo/src/main/java/com/CLASEANADIS/anadis/LectorJSON.java
+++ BACK_APIComplejo/src/main/java/com/CLASEANADIS/anadis/LectorJSON.java
@@ -10,5 +10,29 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class LectorJSON {
+    public DatosJSONcomplejoPADRE LecturaJSONcomplejo(){
+        String json;
+        DatosJSONcomplejoPADRE datosJSONcomplejoPADRE = new DatosJSONcomplejoPADRE();
 
+        Gson gson = new Gson();
+
+        json = "";
+
+        try (BufferedReader br = new BufferedReader(new FileReader("JSONip_Complejo.json"))){
+            String linea;
+
+            while ((linea = br.readLine()) != null){
+                json+= linea;
+                //System.out.println(linea);
+            }
+
+            datosJSONcomplejoPADRE = gson.fromJson(json, DatosJSONcomplejoPADRE.class);
+            System.out.println("Sesion guardada con exito");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return datosJSONcomplejoPADRE;
+    }
 }
